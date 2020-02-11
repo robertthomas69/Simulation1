c66f255916d4c926d3bec26da46bf8e6cf18faf0
robertthomas69 <robertthomas@amdg.rockhursths.edu>
2020-02-11 09:52:18 -0600

    added dice rolls

---
+++ dice.py
@@ -0,0 +1,20 @@
+import random
+import array
+roll = array.array('i', [0])
+n = int(input("Enter number of Dice:"))
+t = int(input("Enter number of Sides:"))
+x = 0
+for i in range(1,t):
+    roll.append(0)
+    def Dicerolls(n)
+
+    for i in range(0,n)
+    x = random.randrange(1,t+1,1)
+    if random.randrange(1,11,1) <= 2:
+        x = 1
+        roll[x-1] = roll[x-1]+1
+
+Dicerolls(n)
+for i in range(0,t):
+    print(str(i+1) + ": " + str(roll[i]) + "/" + str(n) + "=" + str((roll[i]/n)*100) + "%")
+    
