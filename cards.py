06cf0ef7c4b1ad85d4f580293129dd8a8f043767
robertthomas69 <robertthomas@amdg.rockhursths.edu>
2020-02-18 08:57:33 -0600

    Cards Butt

--- cards.py
+++ cards.py
@@ -1,4 +1,26 @@
-for i in range(5):
-    for j in range(5):
-        print([i, j])
-        
+deck = [] 
+suits = ["♠,♣,♥,♦"]
+
+for i in range(4):
+    for j in range(1,14):
+        value = str(j)
+        if(j == 1):
+            value = "A"
+        if(j == 11):
+            value = "J"
+        if(j == 12):
+            value = "Q"
+        if(j == 13):
+            value = "K"
+        deck.append(f"{value}{suits[i]}")
+print(deck)
+
+for i in range(len(deck)-1, 0, -1):
+
+    j = random.randint(0, i + 1):
+
+    deck[i], deck[j] = deck[j], deck[i]
+
+print("The shuffled dock is : " + str(deck))
+
+
